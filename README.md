09be8e29c0f221980f1706562a95ee43bffbb91f
Forrest Olson <fs.olson5@gmail.com>
2022-06-17 23:26:22 -0600

    cleaning up the code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # csi-assessment
+
 A little project to showcase certain skills in development, like utilizing Node and Docker.
--- main.js
+++ main.js
@@ -11,16 +11,17 @@ async function extractArchive(filepath) {
     zip.extractAllTo(outputDir);
     console.log(`Extracted to "${outputDir}" successfully!`);
     // REF: https://medium.com/stackfame/get-list-of-all-files-in-a-directory-in-node-js-befd31677ec5
-    fs.readdir(outputDir, function (err, files) {
+    fs.readdir(outputDir, (err, files) => {
       //handling error
       if (err) {
           return console.log('Unable to scan directory: ' + err);
       } 
-      //listing all files using forEach
-      let filesReverse = files.slice().reverse()
-      filesReverse.forEach(file => console.log(file));
+      // reverse order
+      let fileListReversed = files.slice().reverse()
+      fileListReversed.forEach(file => console.log(file));
+      // normal order
       // files.forEach(file => console.log(file));
-  });
+    });
   } catch (e) {
     console.log(`Something went wrong. ${e}`);
   }
@@ -28,13 +29,13 @@ async function extractArchive(filepath) {
 
 // REF: https://stackoverflow.com/questions/11944932/how-to-download-a-file-with-node-js-without-using-third-party-libraries
 const file = fs.createWriteStream("Photos_To_Review.zip");
-const request = https.get("https://downloads.campbellcloud.io/assessment/202009/Photos_To_Review.zip", function(response) {
-   response.pipe(file);
+https.get("https://downloads.campbellcloud.io/assessment/202009/Photos_To_Review.zip", (response) => {
+  response.pipe(file);
 
-   // after download completed close filestream
-   file.on("finish", () => {
-       file.close();
-       console.log("Download Completed");
-       extractArchive("./Photos_To_Review.zip")
-   });
+  // after download completed close filestream
+  file.on("finish", () => {
+    file.close();
+    console.log("Download Completed");
+    extractArchive("./Photos_To_Review.zip")
+  });
 });
