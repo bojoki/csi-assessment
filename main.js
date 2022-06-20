16cb9ae63bcf9249244e94ed2feeec7ddde9ae6b
Forrest Olson <fs.olson5@gmail.com>
2022-06-20 10:38:50 -0600

    refactor

--- main.js
+++ main.js
@@ -10,32 +10,48 @@ async function extractArchive(filepath) {
     const outputDir = `${path.parse(filepath).name}_extracted`;
     zip.extractAllTo(outputDir);
     console.log(`Extracted to "${outputDir}" successfully!`);
+    return outputDir;
+
+  } catch (e) {
+    console.log(`Something went wrong. ${e}`);
+  }
+}
+
+function readFolder(foldername) {
     // REF: https://medium.com/stackfame/get-list-of-all-files-in-a-directory-in-node-js-befd31677ec5
-    fs.readdir(outputDir, (err, files) => {
+    fs.readdir("./"+foldername, (err, files) => {
       //handling error
       if (err) {
           return console.log('Unable to scan directory: ' + err);
       } 
       // reverse order
       let fileListReversed = files.slice().reverse()
-      fileListReversed.forEach(file => console.log(file));
+      fileListReversed.forEach(filename => console.log(filename));
       // normal order
       // files.forEach(file => console.log(file));
     });
-  } catch (e) {
-    console.log(`Something went wrong. ${e}`);
-  }
 }
-
-// REF: https://stackoverflow.com/questions/11944932/how-to-download-a-file-with-node-js-without-using-third-party-libraries
-const file = fs.createWriteStream("Photos_To_Review.zip");
-https.get("https://downloads.campbellcloud.io/assessment/202009/Photos_To_Review.zip", (response) => {
-  response.pipe(file);
+function downloadResource(resource) {}
+function saveResource(filename, resource) {
+  const file = fs.createWriteStream(filename);
+  resource.pipe(file);
 
   // after download completed close filestream
-  file.on("finish", () => {
+  file.on("finish", async () => {
     file.close();
     console.log("Download Completed");
-    extractArchive("./Photos_To_Review.zip")
+    let outputDirectory = await extractArchive(filename)
+    readFolder(outputDirectory)
   });
-});
+}
+
+async function main() {
+  // REF: https://stackoverflow.com/questions/11944932/how-to-download-a-file-with-node-js-without-using-third-party-libraries
+  https.get("https://downloads.campbellcloud.io/assessment/202009/Photos_To_Review.zip", (response) => {
+    let filename = "Photos_To_Review.zip"
+    saveResource(filename, response)
+  
+  });
+}
+
+main();
